e482606d744a01a44445211075c51bead72421e5
罗祖邦 <luozubang@wondersgroup.com>
2018-12-20 12:31:04 +0800

    springcloud之feign

---
+++ cloudday3_feign/serice-feign/src/main/java/com/arryluo/sericefeign/clients/SchedualServiceHi.java
@@ -0,0 +1,16 @@
+package com.arryluo.sericefeign.clients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author lzb
+ */
+@FeignClient(value = "eureka-client")
+public interface SchedualServiceHi {
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
+
